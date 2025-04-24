3c842aa15fc1930e28499cbbcfd79adc165c62d5
Jose Fernando Ospina <jfernando2266@gmail.com>
2025-04-24 08:02:12 -0500

    Delete ejercicio_9.py

--- ejercicio_9.py
+++
@@ -1,6 +0,0 @@
-min=float(input("ingrese el tiempo en minutos: "))
-
-horas= min/60
-dias= min /1440
-minf= min - int(1440*dias)
-print("dias : ",round(dias,2), ", horas :" , round(horas,2), ", minutos :", round(minf,2))
