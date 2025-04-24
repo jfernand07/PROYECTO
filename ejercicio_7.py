b2ffadc9eec17e674fa5e7566ea8b4fac363cb46
Jose Fernando Ospina <jfernando2266@gmail.com>
2025-04-24 08:01:53 -0500

    Delete ejercicio_7.py

--- ejercicio_7.py
+++
@@ -1,8 +0,0 @@
-numero = input("Introduce un número de cuatro cifras: ")
-
-cifra1 = int(numero[0])
-cifra2 = int(numero[1])
-cifra3 = int(numero[2])
-cifra4 = int(numero[3])
-
-print("El número es:", cifra1, ", ", cifra2, ", ", cifra3, ", ", cifra4)
