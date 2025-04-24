625c7203090eff8fb2acb08504d6523fcdbbad94
Jose Fernando Ospina <jfernando2266@gmail.com>
2025-04-24 08:01:24 -0500

    Delete ejercicio_4.py

--- ejercicio_4.py
+++
@@ -1,9 +0,0 @@
-#Fecha Formateada
-
-try: 
-    dia=int(input("ingresa dia"))
-    mes=int(input("ingresa mes"))
-    año=int(input("ingresa año"))
-    print (dia,"/",mes,"/",año)
-except ValueError: 
-    print ("solo numero enteros")
