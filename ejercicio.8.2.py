da8ff18afbfe27de8cedd73bbd795c0ce5a9553c
Jose Fernando Ospina <jfernando2266@gmail.com>
2025-04-24 20:38:34 -0500

    Update ejercicio.8.2.py

---
+++ ejercicio.8.2.py
@@ -0,0 +1,7 @@
+#division entera y modulo
+numb1=int(input("Introduce el primer numero: "))
+numb2=int(input("Introduce el segundo numero: "))   
+Ente=numb1//numb2
+modulo=numb1%numb2
+print("La division entera de",numb1,"entre",numb2,"es:",Ente)
+print("El modulo de",numb1,"entre",numb2,"es:",modulo)
