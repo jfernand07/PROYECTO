469058aa5a9480b1ef15eb95be8c5463a238f05b
Jose Fernando Ospina <jfernando2266@gmail.com>
2025-04-25 06:59:23 -0500

    Update ejercicio.10.2.py

---
+++ ejercicio.10.2.py
@@ -0,0 +1,5 @@
+# Potencias con f-strings
+numb=int(input(f"ingresa un numero : "))
+result= numb**2
+result1= numb**3
+print(("el resultado del numero elevadoa la 2 es : "), result, ("y el resultado elevado a la 3 es: "), result1 )
